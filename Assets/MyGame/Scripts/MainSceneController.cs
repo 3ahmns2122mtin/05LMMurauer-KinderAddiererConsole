799866214545179ebc290e87d9c71377bd3ba475
LMMurauer <lisa.murauer04@htl-salzburg.ac.at>
2021-12-23 10:28:22 +0100

    init

---
+++ Assets/MyGame/Scripts/MainSceneController.cs
@@ -0,0 +1,54 @@
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneController : MonoBehaviour
+{
+    [SerializeField] private InputField inputFieldTermA, inputFieldTermB;
+    [SerializeField] private Text txtResult;
+    private int termA, termB;
+
+    public int CheckAddition(int summandA, int summandB)
+    {
+        int result = 0;
+
+        result = summandA + summandB;
+
+        Debug.Log(result);
+        txtResult.text = string.Format("{0}", result);
+        return result;
+    }
+    void Start()
+    {
+        Debug.Log("Frohe Weihnachten");
+    }
+
+    public void GetValues()
+    {
+        if(Regex.IsMatch(inputFieldTermA.text, @"^\d+$") && Regex.IsMatch(inputFieldTermB.text, @"^\d+$")) 
+        //überprüft ob in den Inputboxxen ein Zahlenwert steht
+        {
+            termA = int.Parse(inputFieldTermA.text);
+            termB = int.Parse(inputFieldTermB.text);
+        }
+        else
+        {
+            termA = 0;
+            termB = 0;
+        }
+        CheckAddition(termA, termB); //ruft die Additionsfunktion auf
+    }
+
+    public void ResetAll()
+    {
+        txtResult.text = "";
+        inputFieldTermA.text = "";
+        inputFieldTermB.text = "";
+        termA = 0;
+        termB = 0;
+    }
+    void Update()
+    {
+
+    }
+}
